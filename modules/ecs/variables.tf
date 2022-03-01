6fc17b6529853d84f56ec362705f19463bef7559
Alexandro DE OLIVEIRA <adeoliveira@vente-privee.com>
2022-03-01 10:17:52 +0100

    move specific input variables from ecs module to asg

--- modules/ecs/variables.tf
+++ modules/ecs/variables.tf
@@ -61,23 +61,6 @@ variable "service_name" {
   description = "Name of the ECS service"
 }
 
-variable "ec2_desired_count" {
-  type        = number
-  default     = 2
-  description = "Number of ec2 instances to be launched by Auto Scaling Group"
-}
-
-variable "asg_min_size" {
-  type        = number
-  default     = 2
-  description = "Minimum size for ec2 Auto Scaling Group"
-}
-
-variable "asg_max_size" {
-  type    = number
-  default = 8
-}
-
 variable "task_family_name" {
   type        = string
   default     = "hello"
@@ -139,6 +122,23 @@ variable "associate_public_ip" {
   type        = bool
   default     = true
   description = "Associate a public ip address with an instance in a VPC"
+}
+
+variable "ec2_desired_count" {
+  type        = number
+  default     = 2
+  description = "Number of ec2 instances to be launched by Auto Scaling Group"
+}
+
+variable "asg_min_size" {
+  type        = number
+  default     = 2
+  description = "Minimum size for ec2 Auto Scaling Group"
+}
+
+variable "asg_max_size" {
+  type    = number
+  default = 8
 }
 
 variable "automation_tag" {
